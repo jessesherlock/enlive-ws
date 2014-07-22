3a691209dba55898e55d8b9a404d466109b6fde7
Jesse Sherlock <jesse@jessesherlock.com>
2014-07-21 18:30:55 -0700

    updated project.clj with url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject enlive-ws "0.1.1-SNAPSHOT"
   :description "enlive additions to strip whitespace"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jessesherlock/enlive-ws"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
